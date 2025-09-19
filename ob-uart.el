56371216e0472b47d08ef7192f757f1327c86fd6
LM Butler <68486806+elemb@users.noreply.github.com>
2025-09-19 14:52:43 -0600

    Update ob-uart.el
    back to "fix" state

--- ob-uart.el
+++ ob-uart.el
@@ -1,16 +1,12 @@
 ;;; ob-uart.el --- org-babel support for UART communication
-
 ;; Copyright (C) Andreas Müller
-
 ;; Author: Andreas Müller
 ;; Keywords: tools, comm, org-mode, UART, literate programming, reproducible development
 ;; Homepage: https://www.0x7.ch
 ;; Version: 0.0.1
-
 ;; code inspired by ob-restclient.el - https://github.com/alf/ob-restclient.el
 
 ;;; License:
-
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3, or (at your option)
@@ -26,9 +22,9 @@
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
-
 ;;; Commentary:
-;;
+;; This package provides org-babel support for UART communication.
+;; It allows executing UART commands directly in org-mode code blocks.
 
 ;;; Code:
 (require 'ob)
@@ -41,15 +37,36 @@
   :group 'org-babel)
 
 (defcustom ob-uart-debug t
-  "Whether to notify in case of new commits."
+  "Whether to show debug messages for UART communication."
   :package-version '(ob-uart . "0.0.1")
   :group 'ob-uart
   :type 'boolean)
 
+(defun ob-uart-detect-default-port ()
+  "Detect default serial port based on operating system."
+  (cond
+   ((eq system-type 'darwin)
+    ;; macOS - look for USB serial devices
+    (let ((ports (append (directory-files "/dev" nil "cu\\.usbmodem.*")
+                        (directory-files "/dev" nil "cu\\.usbserial.*"))))
+      (if ports
+          (concat "/dev/" (car ports))
+        "/dev/cu.usbmodem")))
+   ((eq system-type 'gnu/linux)
+    ;; Linux
+    (let ((ports (append (directory-files "/dev" nil "ttyUSB.*")
+                        (directory-files "/dev" nil "ttyACM.*"))))
+      (if ports
+          (concat "/dev/" (car ports))
+        "/dev/ttyUSB0")))
+   (t
+    ;; Default fallback
+    "/dev/ttyUSB0")))
+
 (defvar org-babel-default-header-args:uart
   `((:ienc . "raw")
     (:oenc . "raw")
-    (:port . "/dev/ttyUSB0")
+    (:port . ,(ob-uart-detect-default-port))
     (:speed . 9600)
     (:bytesize . 8)
     (:parity . nil)
@@ -59,73 +76,101 @@
     (:lineend . "\n"))
   "Default arguments for evaluating a UART block.")
 
-;;;###aut
+;;;###autoload
 (defun org-babel-execute:uart (body params)
-  "Execute a block of Lemonbeat code with org-babel.
+  "Execute a block of UART code with org-babel.
 This function is called by `org-babel-execute-src-block'
 Argument BODY content to send.
 Argument PARAMS UART communication parameters."
   (message "executing UART source code block")
-  (let* ((ienc (cdr (assoc :ienc params)))
-	 (oenc (cdr (assoc :oenc params)))
-	 (port (cdr (assoc :port params)))
-	 (speed (cdr (assoc :speed params)))
-	 (bytesize (cdr (assoc :bytesize params)))
-	 (parity (cdr (assoc :parity params)))
-	 (stopbits (cdr (assoc :stopbits params)))
-	 (flowcontrol (cdr (assoc :flowcontrol params)))
-	 (timeout (cdr (assoc :timeout params)))
-	 (lineend (cdr (assoc :lineend params)))
-	 (process (format "ob-uart-%s" port))
-	 (process-buffer (format "*ob-uart-%s*" port)))
-
-    (make-serial-process
-     :name process
-     :buffer process-buffer
-     :speed 115200
-     :port port
-     :speed speed
-     :bytesize bytesize
-     :parity parity
-     :stopbits stopbits
-     :flowcontrol flowcontrol
-     :filter 'ob-uart-listen-filter)
-
-    (when (string= "hex" ienc)
-	(setq body (mapconcat (lambda (x) (byte-to-string (string-to-number x 16))) (split-string body) "")))
-
-    (process-send-string process (concat body lineend))
-    (with-current-buffer process-buffer
-      (erase-buffer))
-
-    (sleep-for timeout)
-
-    (let ((result))
+  (let* ((processed-params (org-babel-process-params params))
+         (ienc (cdr (assoc :ienc processed-params)))
+         (oenc (cdr (assoc :oenc processed-params)))
+         (port (cdr (assoc :port processed-params)))
+         (speed (string-to-number (or (cdr (assoc :speed processed-params)) "9600")))
+         (bytesize (string-to-number (or (cdr (assoc :bytesize processed-params)) "8")))
+         (parity (let ((p (cdr (assoc :parity processed-params))))
+                   (cond ((string= p "odd") 'odd)
+                         ((string= p "even") 'even)
+                         (t nil))))
+         (stopbits (string-to-number (or (cdr (assoc :stopbits processed-params)) "1")))
+         (flowcontrol (let ((fc (cdr (assoc :flowcontrol processed-params))))
+                        (cond ((string= fc "hw") 'hw)
+                              ((string= fc "sw") 'sw)
+                              (t nil))))
+         (timeout (string-to-number (or (cdr (assoc :timeout processed-params)) "1")))
+         (lineend (or (cdr (assoc :lineend processed-params)) "\n"))
+         (process-name (format "ob-uart-%s" (replace-regexp-in-string "/" "-" port)))
+         (process-buffer (format "*ob-uart-%s*" (replace-regexp-in-string "/" "-" port))))
+    
+    ;; Clean up any existing process and buffer
+    (when (get-process process-name)
+      (delete-process process-name))
+    (when (get-buffer process-buffer)
+      (kill-buffer process-buffer))
+    
+    ;; Create the serial process
+    (let ((proc (make-serial-process
+                 :name process-name
+                 :buffer process-buffer
+                 :port port
+                 :speed speed
+                 :bytesize bytesize
+                 :parity parity
+                 :stopbits stopbits
+                 :flowcontrol flowcontrol
+                 :filter 'ob-uart-listen-filter)))
+      
+      ;; Process input encoding
+      (when (string= "hex" ienc)
+        (setq body (mapconcat 
+                   (lambda (x) 
+                     (char-to-string (string-to-number x 16))) 
+                   (split-string body) "")))
+      
+      ;; Send the command
+      (process-send-string proc (concat body lineend))
+      
+      ;; Clear buffer and wait for response
       (with-current-buffer process-buffer
-	(setq result (buffer-string)))
-      (delete-process process)
-      (kill-buffer process-buffer)
-
-      (when (string= "hex" oenc)
-	  (setq result (mapconcat (lambda (x) (format "%02x" x)) (string-to-list result) " ")))
-
-      (when (string= "HEX" oenc)
-	  (setq result (mapconcat (lambda (x) (format "%02X" x)) (string-to-list result) " ")))
-
-      (with-temp-buffer
-	(insert result)
-	(when (not (string= "raw" oenc))
-	    (fill-region (point-min) (point-max)))
-	(buffer-string)))))
+        (erase-buffer))
+      (sleep-for timeout)
+      
+      ;; Get result
+      (let ((result ""))
+        (with-current-buffer process-buffer
+          (setq result (buffer-string)))
+        
+        ;; Clean up
+        (delete-process proc)
+        (kill-buffer process-buffer)
+        
+        ;; Process output encoding
+        (when (string= "hex" oenc)
+          (setq result (mapconcat (lambda (x) (format "%02x" x)) 
+                                 (string-to-list result) " ")))
+        (when (string= "HEX" oenc)
+          (setq result (mapconcat (lambda (x) (format "%02X" x)) 
+                                 (string-to-list result) " ")))
+        
+        ;; Format result
+        (with-temp-buffer
+          (insert result)
+          (when (not (string= "raw" oenc))
+            (fill-region (point-min) (point-max)))
+          (buffer-string))))))
 
 (defun ob-uart-listen-filter (proc string)
   "Filter to process response.
 Argument PROC process.
 Argument STRING response string."
   (when ob-uart-debug
-      (message (format "ob-uart got %d bytes" (string-bytes string))))
-  (with-current-buffer (format "*%s*" (process-name proc))
+    (message (format "ob-uart got %d bytes" (length string))))
+  (with-current-buffer (process-buffer proc)
     (insert string)))
+
+;; Add uart to the list of supported languages
+(add-to-list 'org-babel-load-languages '(uart . t))
 
 (provide 'ob-uart)
 ;;; ob-uart.el ends here
