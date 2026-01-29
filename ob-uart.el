bc438613a2eb2a639b4cb3fc301e6096c947393b
LM Butler <68486806+elemb@users.noreply.github.com>
2026-01-28 20:27:23 -0700

    Rename ob-uart to ob-cereal for serial support

--- ob-uart.el
+++ ob-uart.el
@@ -1,15 +1,13 @@
-;;; ob-uart.el --- org-babel support for UART communication
+;;; ob-cereal.el --- org-babel support for serial communication
 
 ;; Copyright (C) Andreas Müller
-
 ;; Original Author and Idea and 100% credit to: Andreas Müller
 ;; Revision Author: elemb
-;; Keywords: tools, comm, org-mode, UART, literate programming, reproducible development
-;; Homepage: https://github.com/elemb/ob-uart
+;; Keywords: tools, comm, org-mode, serial, literate programming, reproducible development
+;; Homepage: https://github.com/elemb/ob-cereal   ;; ← update to your fork URL
 ;; Version: 0.1.0
 
 ;;; License:
-
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3, or (at your option)
@@ -26,22 +24,22 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
-;; Org-babel support for UART/serial communication with embedded devices.
+;; Org-babel support for serial communication with embedded devices.
 ;; Supports Forth systems like Mecrisp and ZeptoForth.
 
 ;;; Code:
 (require 'ob)
 
-(defgroup ob-uart nil
-  "UART support for org babel."
+(defgroup ob-cereal nil
+  "Serial communication support for org babel."
   :group 'org-babel)
 
-(defcustom ob-uart-debug nil
-  "Whether to show debug messages for UART communication."
-  :group 'ob-uart
+(defcustom ob-cereal-debug nil
+  "Whether to show debug messages for serial communication."
+  :group 'ob-cereal
   :type 'boolean)
 
-(defvar org-babel-default-header-args:uart
+(defvar org-babel-default-header-args:cereal
   '((:port . "/dev/ttyUSB0")
     (:speed . 115200)
     (:bytesize . 8)
@@ -52,20 +50,18 @@
     (:lineend . "\n")
     (:ienc . "raw")
     (:oenc . "raw"))
-  "Default arguments for evaluating a UART block.")
+  "Default arguments for evaluating a cereal block.")
 
-(defun ob-uart--to-number (val default)
+(defun ob-cereal--to-number (val default)
   "Convert VAL to number. If already number, return it. If string, convert. Else DEFAULT."
   (cond
    ((numberp val) val)
    ((and (stringp val) (not (string-empty-p val)))
     (let ((num (string-to-number val)))
-      (if (zerop num)
-          default
-        num)))
+      (if (zerop num) default num)))
    (t default)))
 
-(defun ob-uart--to-string (val)
+(defun ob-cereal--to-string (val)
   "Convert VAL to string if it's a symbol, otherwise return as-is."
   (cond
    ((stringp val) val)
@@ -73,64 +69,44 @@
    ((numberp val) (number-to-string val))
    (t (format "%s" val))))
 
-(defun ob-uart--to-symbol (val valid-syms)
+(defun ob-cereal--to-symbol (val valid-syms)
   "Convert VAL to symbol if in VALID-SYMS, else nil."
   (let ((sym (cond
               ((symbolp val) val)
-              ((stringp val) 
+              ((stringp val)
                (let ((s (downcase val)))
-                 (cond
-                  ((member s '("nil" "none")) nil)
-                  (t (intern s)))))
+                 (cond ((member s '("nil" "none")) nil)
+                       (t (intern s)))))
               (t nil))))
-    (if (memq sym valid-syms)
-        sym
-      nil)))
+    (if (memq sym valid-syms) sym nil)))
 
 ;;;###autoload
-(defun org-babel-execute:uart (body params)
-  "Execute UART code block with BODY and PARAMS."
-  (when ob-uart-debug
-    (message "ob-uart executing with params: %S" params))
-  
-  (let* ((port (ob-uart--to-string 
-                (or (cdr (assq :port params)) "/dev/ttyUSB0")))
-         (speed (ob-uart--to-number 
-                 (cdr (assq :speed params)) 115200))
-         (bytesize (ob-uart--to-number 
-                    (cdr (assq :bytesize params)) 8))
-         (parity (ob-uart--to-symbol 
-                  (cdr (assq :parity params))
-                  '(odd even)))
-         (stopbits (ob-uart--to-number 
-                    (cdr (assq :stopbits params)) 1))
-         (flowcontrol (ob-uart--to-symbol 
-                       (cdr (assq :flowcontrol params))
-                       '(hw sw)))
-         (timeout (ob-uart--to-number 
-                   (cdr (assq :timeout params)) 2))
-         (lineend (ob-uart--to-string 
-                   (or (cdr (assq :lineend params)) "\n")))
-         (ienc (ob-uart--to-string 
-                (or (cdr (assq :ienc params)) "raw")))
-         (oenc (ob-uart--to-string 
-                (or (cdr (assq :oenc params)) "raw")))
-         (proc-name (format "ob-uart-%s" 
-                            (replace-regexp-in-string "[/:]" "-" port)))
-         (proc-buf (format " *ob-uart-%s*" 
-                           (replace-regexp-in-string "[/:]" "-" port))))
-    
-    (when ob-uart-debug
-      (message "ob-uart: port=%s speed=%d bytesize=%d parity=%s stopbits=%d flow=%s timeout=%d"
+(defun org-babel-execute:cereal (body params)
+  "Execute serial code block with BODY and PARAMS."
+  (when ob-cereal-debug
+    (message "ob-cereal executing with params: %S" params))
+
+  (let* ((port      (ob-cereal--to-string (or (cdr (assq :port      params)) "/dev/ttyUSB0")))
+         (speed     (ob-cereal--to-number (cdr (assq :speed     params)) 115200))
+         (bytesize  (ob-cereal--to-number (cdr (assq :bytesize  params)) 8))
+         (parity    (ob-cereal--to-symbol (cdr (assq :parity    params)) '(odd even)))
+         (stopbits  (ob-cereal--to-number (cdr (assq :stopbits  params)) 1))
+         (flowcontrol (ob-cereal--to-symbol (cdr (assq :flowcontrol params)) '(hw sw)))
+         (timeout   (ob-cereal--to-number (cdr (assq :timeout   params)) 2))
+         (lineend   (ob-cereal--to-string (or (cdr (assq :lineend   params)) "\n")))
+         (ienc      (ob-cereal--to-string (or (cdr (assq :ienc      params)) "raw")))
+         (oenc      (ob-cereal--to-string (or (cdr (assq :oenc      params)) "raw")))
+         (proc-name (format "ob-cereal-%s" (replace-regexp-in-string "[/:]" "-" port)))
+         (proc-buf  (format " *ob-cereal-%s*" (replace-regexp-in-string "[/:]" "-" port))))
+
+    (when ob-cereal-debug
+      (message "ob-cereal: port=%s speed=%d bytesize=%d parity=%s stopbits=%d flow=%s timeout=%d"
                port speed bytesize parity stopbits flowcontrol timeout))
-    
-    ;; Clean up existing process
-    (when (get-process proc-name)
-      (delete-process proc-name))
-    (when (get-buffer proc-buf)
-      (kill-buffer proc-buf))
-    
-    ;; Execute with error handling
+
+    ;; Clean up any stale process/buffer
+    (when (get-process proc-name)   (delete-process proc-name))
+    (when (get-buffer proc-buf)     (kill-buffer proc-buf))
+
     (condition-case err
         (let ((proc (make-serial-process
                      :name proc-name
@@ -141,58 +117,42 @@
                      :parity parity
                      :stopbits stopbits
                      :flowcontrol flowcontrol)))
-          
-          ;; Encode input if needed
+
+          ;; Encode input if hex mode
           (let ((encoded-body body))
             (when (string= ienc "hex")
-              (setq encoded-body 
-                    (mapconcat
-                     (lambda (hex)
-                       (char-to-string (string-to-number hex 16)))
-                     (split-string body "[ \t\n]+" t)
-                     "")))
-            
-            ;; Send data
-            (process-send-string proc (concat encoded-body lineend))
-            
-            ;; Wait for response using non-blocking I/O
-            (let ((start-time (float-time))
-                  (elapsed 0))
-              (while (and (< elapsed timeout)
-                         (process-live-p proc))
-                (accept-process-output proc 0.1 nil t)
-                (setq elapsed (- (float-time) start-time))))
-            
-            ;; Get result
-            (let ((result (with-current-buffer proc-buf
-                           (buffer-string))))
-              
-              ;; Clean up
-              (when (process-live-p proc)
-                (delete-process proc))
-              (when (get-buffer proc-buf)
-                (kill-buffer proc-buf))
-              
-              ;; Strip control characters (except newlines and tabs)
-              (setq result (replace-regexp-in-string "[\x00-\x08\x0B-\x1F\x7F]" "" result))
-              
-              ;; Decode output if needed
-              (when (string= oenc "hex")
-                (setq result 
-                      (mapconcat 
-                       (lambda (c) (format "%02x" c))
-                       result " ")))
-              (when (string= oenc "HEX")
-                (setq result 
-                      (mapconcat 
-                       (lambda (c) (format "%02X" c))
-                       result " ")))
-              
-              result)))
-      
-      (error
-       (format "Error in UART communication: %s" 
-               (error-message-string err))))))
+              (setq encoded-body
+                    (mapconcat (lambda (hex) (char-to-string (string-to-number hex 16)))
+                               (split-string body "[ \t\n]+" t) "")))
 
-(provide 'ob-uart)
-;;; ob-uart.el ends here
+            ;; Send
+            (process-send-string proc (concat encoded-body lineend))
+
+            ;; Wait (non-blocking)
+            (let ((start (float-time)) (elapsed 0))
+              (while (and (< elapsed timeout) (process-live-p proc))
+                (accept-process-output proc 0.1 nil t)
+                (setq elapsed (- (float-time) start))))
+
+            ;; Capture & clean result
+            (let ((result (with-current-buffer proc-buf (buffer-string))))
+              ;; Clean up
+              (when (process-live-p proc) (delete-process proc))
+              (when (get-buffer proc-buf) (kill-buffer proc-buf))
+
+              ;; Strip control chars (keep \n and \t)
+              (setq result (replace-regexp-in-string "[\x00-\x08\x0B-\x1F\x7F]" "" result))
+
+              ;; Optional hex output decoding
+              (cond
+               ((string= oenc "hex")
+                (setq result (mapconcat (lambda (c) (format "%02x" c)) result " ")))
+               ((string= oenc "HEX")
+                (setq result (mapconcat (lambda (c) (format "%02X" c)) result " "))))
+
+              result)))
+
+      (error (format "Error in serial communication: %s" (error-message-string err))))))
+
+(provide 'ob-cereal)
+;;; ob-cereal.el ends here
